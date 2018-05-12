fad3aec0b28c3419e97cd4a3e08ae29b312770e2
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2018-05-12 15:45:22 +0200

    npm http-server is a lot faster, trying to bundle it

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ virtualenv venv/
 source venv/bin/activate
 pip install -r requirements_venv.txt
 cp mpld3_fix/_display.py venv/lib/python2.7/site-packages/mpld3/
-nodeenv -p
-npm install http-server -g
+nodeenv -p --requirements=requirements_venv_nodejs.txt --jobs=4 --force venv
 deactivate
