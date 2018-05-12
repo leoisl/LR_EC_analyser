2dd4d421f09cfe42f2f917f6236ce8fa34b475f4
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2018-05-12 16:16:23 +0200

    Updating the serve_files script with http-server now

--- serve_files.py
+++ serve_files.py
@@ -2,39 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """
-Creates a HTTP file server accepting CORS and POST
-99% Based on https://gist.github.com/jlesquembre/3922805 (all I did was refactoring it a little bit)
+Creates a HTTP file server using npm http-server
 """
 
-from twisted.web.server import Site
-from twisted.web.static import File
-from twisted.internet import reactor
 import argparse
+from ExternalTools import executeCommandLine
 
-class ResponseFile(File):
-    def render(self, request):
-        print request
-        request.setHeader('Access-Control-Allow-Origin', '*')
-        request.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-        request.setHeader('Access-Control-Allow-Credentials', 'true')
-        request.setHeader('Access-Control-Allow-Headers', 'origin, x-requested-with, accept, content-type, range')
+parser = argparse.ArgumentParser(description='Serve files rooted at the output parameter. Uses npm http-server')
+parser.add_argument("output", help="Output folder served.")
+parser.add_argument("--port", type=int, help="The port to use", default=19974)
+args=parser.parse_args()
 
-        return File.render(self, request)
-
-    def render_OPTIONS(self, request):
-        return self.render_GET(request)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Serve files rooted at the output parameter. Accepts CORS and POST.')
-    parser.add_argument("output", help="Output folder served.")
-    parser.add_argument("--port", type=int, help="The port to use", default=19974)
-    args=parser.parse_args()
-
-    root = ResponseFile(args.output)
-    reactor.listenTCP(args.port, Site(root))
-    print "Server is starting, now you can see the IGV plots"
-    reactor.run()
-
-
-
+try:
+    executeCommandLine("http-server --cors -p %d %s"%(args.port, args.output))
+except KeyboardInterrupt:
+    pass
