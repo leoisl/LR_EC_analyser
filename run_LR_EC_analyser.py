a8c3160de9c1f61fc9faa5ca7dc2f3ae4ad52d20
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-01-10 14:48:56 +0100

    Fixed a bug with the colours parameter

--- run_LR_EC_analyser.py
+++ run_LR_EC_analyser.py
@@ -77,7 +77,8 @@ def main():
         for i, colour in enumerate(args.colours):
             if colour[0]!="#":
                 args.colours[i] = "#"+colour
-
+    else:
+        args.colours = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 
     #copy genome to output and index it
     genome = args.output + "/" + os.path.basename(args.genome)
