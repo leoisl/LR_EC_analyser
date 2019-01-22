c61f78507089a63fc635a3954abf28eaf66498a6
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-01-22 18:44:46 +0100

    Generating eps images

--- Plotter.py
+++ Plotter.py
@@ -42,6 +42,9 @@ class Plotter:
             height=600
 
         plotly.io.write_image(fig, "%s/%s.png"%(self.plotsOutput, name), scale=2.0, width=width, height=height)
+
+        #uncomment this if you want to create .eps figures required for the paper
+        plotly.io.write_image(fig, "%s/%s.eps" % (self.plotsOutput, name), width=width, height=height)
         fig["layout"]["hovermode"] = "closest"
 
 
