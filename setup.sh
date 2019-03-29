aa348c4baeb3ac80b35730f9dbd03813a6f0589d
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-03-29 23:45:33 +0100

    Trying an orca patch (#2)
    https://github.com/plotly/orca/issues/140

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ virtualenv venv/
 source venv/bin/activate
 pip install -r requirements_venv.txt
 nodeenv -p --requirements=requirements_venv_nodejs.txt --jobs=4 --force venv
+yes | cp -f orca_connection_refused_fix/create-index.js venv/lib/node_modules/orca/src/util/
 deactivate
